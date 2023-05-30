298665f78ffc15ac046020383aba8fe3bd59d980
Teuku Nabil <nabilmuhammad246@gmail.com>
2023-05-30 23:54:11 +0700

    login 95%

--- app/src/main/java/com/example/sootheme/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/sootheme/ui/RegisterActivity.kt
@@ -4,27 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.sootheme.databinding.ActivityLoginBinding
-import com.airbnb.lottie.LottieAnimationView
-import com.example.sootheme.R
+import com.example.sootheme.databinding.ActivityRegisterBinding
 
-class LoginActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
+    private lateinit var binding : ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.tvToRegister.setOnClickListener {
-            val intent = Intent(this, RegisterActivity::class.java)
+        binding.tvToLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
-
-        binding.loginButton.setOnClickListener {
-            loadingState(true)
         }
     }
 
